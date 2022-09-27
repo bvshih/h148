73b0333782db8d883854d50c55dfb5229de12370
Beverly <bv.shih@gmail.com>
2022-09-27 14:36:21 -0500

    script modified to use parallelisation

--- batchjobs/h148_db_script_5.sh
+++ batchjobs/h148_db_script_5.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #----------------------------------------------------
-#More properties for h148
+#More properties for h148 run in parallel 
 #----------------------------------------------------
-#SBATCH -J h148_db_5           # Job name
-#SBATCH -o ./script_outs/outs/h148_db_5.o%j       # Name of stdout output file
+#SBATCH -J h148_db_5                                # Job name
+#SBATCH -o ./script_outs/outs/h148_db_5.o%j         # Name of stdout output file
 #SBATCH -e ./script_outs/errors/h148_db_5.e%j       # Name of stderr error file
-#SBATCH -p normal          # Queue (partition) name
-#SBATCH -N 1               # Total # of nodes (must be 1 for serial)
-#SBATCH -n 1               # Total # of mpi tasks (should be 1 for serial)
-#SBATCH -t 10:00:00        # Run time (hh:mm:ss)
+#SBATCH -p normal                                   # Queue (partition) name
+#SBATCH -N 4                                        # Total # of nodes 
+#SBATCH -n 8                                        # Total # of mpi tasks 
+#SBATCH -t 10:00:00                                 # Run time (hh:mm:ss)
 #SBATCH --mail-user=bshih0@uw.edu
-#SBATCH --mail-type=all    # Send email at begin and end of job
+#SBATCH --mail-type=all                             # Send email at begin and end of job
 
 # Other commands must follow all #SBATCH directives...
 
@@ -18,8 +18,8 @@ module list
 pwd
 date
 
-# Launch serial code...
+# Launch MPI code...
 
-tangos write dm_density_profile —with-prerequisites --include-only="contamination_fraction<0.01" --for h148.cosmo50PLKvdXsec.6144.VTS
+ibrun tangos write dm_density_profile --with-prerequisites --include-only="contamination_fraction<0.01" --for h148.cosmo50PLKvdXsec.6144.VTS --load-mode=server --backend mpi4py
 
 # ---------------------------------------------------
