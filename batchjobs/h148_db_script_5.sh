93791c2173c53662efc8297eabac5fb6c6c3050f
Beverly <bv.shih@gmail.com>
2022-10-10 14:18:00 -0500

    Changed number of nodes used

--- batchjobs/h148_db_script_5.sh
+++ batchjobs/h148_db_script_5.sh
@@ -5,10 +5,10 @@
 #SBATCH -J h148_db_5                                # Job name
 #SBATCH -o ./script_outs/outs/h148_db_5.o%j         # Name of stdout output file
 #SBATCH -e ./script_outs/errors/h148_db_5.e%j       # Name of stderr error file
-#SBATCH -p normal                                   # Queue (partition) name
-#SBATCH -N 4                                        # Total # of nodes 
-#SBATCH -n 8                                        # Total # of mpi tasks 
-#SBATCH -t 10:00:00                                 # Run time (hh:mm:ss)
+#SBATCH -p skx-normal                                 # Queue (partition) name
+#SBATCH -N 1                                        # Total # of nodes 
+#SBATCH -n 32                                        # Total # of mpi tasks 
+#SBATCH -t 48:00:00                                 # Run time (hh:mm:ss)
 #SBATCH --mail-user=bshih0@uw.edu
 #SBATCH --mail-type=all                             # Send email at begin and end of job
 
@@ -17,9 +17,10 @@
 module list
 pwd
 date
+echo 'N = 1, n = 32'
 
 # Launch MPI code...
 
-ibrun tangos write dm_density_profile --with-prerequisites --include-only="contamination_fraction<0.01" --for h148.cosmo50PLKvdXsec.6144.VTS --load-mode=server --backend mpi4py
+ibrun tangos write dm_density_profile --with-prerequisites --include-only="contamination_fraction<0.01" --for h148.cosmo50PLKvdXsec.6144.VTS --load-mode=server --backend mpi4py --backwards 
 
 # ---------------------------------------------------
