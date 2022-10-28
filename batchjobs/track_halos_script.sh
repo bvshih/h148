6af761bf67b393967b5ac8b873362e8ea79f3fbb
Beverly <bv.shih@gmail.com>
2022-10-28 13:28:07 -0500

    fixes path for outfiles

--- batchjobs/track_halos_script.sh
+++ batchjobs/track_halos_script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # ---------------------------------------------------
 #SBATCH -J track_halos           # Job name
-#SBATCH -o ./script_outs/outs/track_halos.o%j       # Name of stdout output file
-#SBATCH -e ./script_outs/errors/track_halos.e%j       # Name of stderr error file
+#SBATCH -o ./script_outs/track_halos/%j.o        # Name of stdout output file
+#SBATCH -e ./script_outs/track_halos/%j.e       # Name of stderr error file
 #SBATCH -p normal          # Queue (partition) name
 #SBATCH -N 1               # Total # of nodes (must be 1 for serial)
 #SBATCH -n 1               # Total # of mpi tasks (should be 1 for serial)
-#SBATCH -t 10:00:00        # Run time (hh:mm:ss)
-#SBATCH --mail-user=bshih0@uw.edu
+#SBATCH -t 24:00:00        # Run time (hh:mm:ss)
+#SBATCH --mail-user=bv.shih@gmail.com
 #SBATCH --mail-type=all    # Send email at begin and end of job
 
 # Other commands must follow all #SBATCH directives...
