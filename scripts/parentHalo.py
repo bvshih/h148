bb09422bdcbaf4c78e5f179d1005b7e93e1c362f
Beverly <bv.shih@gmail.com>
2022-12-16 19:38:51 -0600

    removes comments used for debugging

--- scripts/parentHalo.py
+++ scripts/parentHalo.py
@@ -2,8 +2,6 @@ from tangos.properties import PropertyCalculation
 from tangos import get_halo
 import numpy as np 
 
-import pandas as pd
-
 class parentHalo(PropertyCalculation):
     names = "parentHalo"
 
@@ -12,41 +10,19 @@ class parentHalo(PropertyCalculation):
 
 
     def calculate(self, particle_data, halo):
-
-        #offsets = np.linalg.norm(np.array([halo['Xc'], halo['Yc'], halo['Zc']]) - self.centres, axis=1)
         offsets = np.linalg.norm(halo['shrink_center'] - self.centres, axis=1)
-        
-        # print('self.centres length:', len(self.centres))
-        # print('self.centres:', self.centres, '\n')
-        # print('existing_properties["shrink_center"]:', existing_properties['shrink_center'],'\n')
-        # print('offsets:', offsets)
 
         offsets[offsets<1e-5] = np.inf # exclude self!
         inside_mask = offsets<self.radii
 
-        # print('offsets length:', len(offsets), '\n')
-        # print('self.radii length:', len(self.radii), '\n')
-
-        # print('inside_mask:', inside_mask, '\n')
-
-        # print('offsets:', offsets)
-
-        # print('self.radii:', self.radii)
-
-        if np.any(inside_mask):      
-            # print('masked rmax:', self.radii[inside_mask])
-            
-            # print('dbid: ', self.dbid[self.radii[inside_mask].argmax()])
+        if np.any(inside_mask):                  
             dbid_masked = self.dbid[inside_mask]
-            # print('masked dbid: ', dbid_masked[self.radii[inside_mask].argmax()])
 
             return get_halo(dbid_masked[self.radii[inside_mask].argmax()]).halo_number()
         else:
             return -1
 
     def preloop(self, particle_data, timestep):
-        #self.xc, self.yc, self.zc , self.radii, self.dbid = timestep.calculate_all('Xc','Yc', 'Zc',"Rhalo","dbid()")
 
-        #self.centres = [self.xc, self.yc, self.zc]
         
         self.centres, self.radii, self.dbid = timestep.calculate_all('shrink_center',"max_radius","dbid()")
